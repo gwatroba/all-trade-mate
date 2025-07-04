554ae42b7b5fb0737525bccbe5fd7338954b5e92
gwatroba <grzegorz.watroba@gmail.com>
2025-07-04 17:12:33 +0200

    updated start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,10 @@ set -e
 
 docker compose up -d
 
+echo "Waiting for Ollama service to start..."
+sleep 5
+
+echo "Pulling llama3 model..."
+docker exec -it ollama ollama pull llama3
+
 echo "Setup complete. All services are running."
