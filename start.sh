1a0b8423af4085ec6210345bb5917d5b7021ec3f
gwatroba <grzegorz.watroba@gmail.com>
2025-07-06 21:40:53 +0200

    renaming project

--- start.sh
+++ start.sh
@@ -3,10 +3,4 @@ set -e
 
 docker compose up -d
 
-echo "Waiting for Ollama service to start..."
-sleep 5
-
-echo "Pulling llama3 model..."
-docker exec -it ollama ollama pull llama3
-
 echo "Setup complete. All services are running."
